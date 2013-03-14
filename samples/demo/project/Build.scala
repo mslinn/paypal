098e5693f28d1909b62c8ebe5eda88f0288bb77e
Mike Slinn <mslinn@micronauticsresearch.com>
2013-03-14 02:34:25 -0700

    Started demo app

---
+++ samples/demo/project/Build.scala
@@ -0,0 +1,20 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+    val appName         = "ssdemo-scala"
+    val appVersion      = "1.0-SNAPSHOT"
+
+    scalaVersion := "2.10.0"
+
+    val appDependencies = Seq(
+    	"com.micronautics" % "securesocial" % "2.1.0" withSources()
+    )
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns),
+      resolvers += Resolver.url("play-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+    )
+
+}
